334e0bf5c006715b878bdaee39d660ddbfa111e4
Matthew Mitchell <matthewprestonmitchell@gmail.com>
2020-06-30 17:49:48 -0700

    fixed data sets

--- Models/Classes.cs
+++ Models/Classes.cs
@@ -5,16 +5,13 @@ namespace MvcWebsite.Models
 {
     public class Classes
     {
-        public int Id { get; set; }
-        public string User { get; set; }
-        public string Class { get; set; }
-        public string UserClass { get; set; }
-
-        [DataType(DataType.Date)]
-        public DateTime SessionDate { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public string Paid { get; set; }
+        public int UserId { get; set; }
+        public int ClassId { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+        public bool UserIsAdmin { get; set; }
+        public string ClassName { get; set; }
+        public string ClassDescription { get; set; }
+        public decimal ClassPrice { get; set; }
     }
 }
