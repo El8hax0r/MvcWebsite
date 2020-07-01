ff4c957a6ef066586409f1f5150773d6a1f585d3
Matthew Mitchell <matthewprestonmitchell@gmail.com>
2020-06-30 17:05:26 -0700

    Add project files.

---
+++ Models/Classes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcWebsite.Models
+{
+    public class Classes
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string Class { get; set; }
+        public string UserClass { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime SessionDate { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Paid { get; set; }
+    }
+}
